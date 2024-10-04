99dd972178e0a1f38a6f83608e90ffd85f1be978
immaculatenamubiru <immaculatenamubiru07@gmail.com>
2024-10-03 23:57:32 -0700

    Add files via upload

---
+++ oop/assignmentdo/src/assignmentdo/dowhileloop.java
@@ -0,0 +1,16 @@
+package assignmentdo;
+
+public class dowhileloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int line = 1;
+
+		// do-while loop
+		do {
+			System.out.println(line);
+			line++;
+		} while (line <=7);
+	}
+
+}
